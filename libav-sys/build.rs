ee730784f5923998f47cba9256c7138fe6355dd8
Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
2020-06-09 19:42:39 +0200

    feat: update bindgen and fix binding errors

--- libav-sys/build.rs
+++ libav-sys/build.rs
@@ -1,48 +1,39 @@
-extern crate bindgen;
-extern crate metadeps;
-
+use std::env;
 use std::fs::File;
 use std::io::Write;
+use std::path::PathBuf;
 
-fn format_write(builder: bindgen::Builder, output: &str) {
-    let s = builder
+fn fmt_write(builder: bindgen::Builder) -> String {
+    builder
         .generate()
         .unwrap()
         .to_string()
+        .replace("pub const FP_NAN: _bindgen_ty_1 = 0;", "")
+        .replace("pub const FP_INFINITE: _bindgen_ty_1 = 1;", "")
+        .replace("pub const FP_ZERO: _bindgen_ty_1 = 2;", "")
+        .replace("pub const FP_SUBNORMAL: _bindgen_ty_1 = 3;", "")
+        .replace("pub const FP_NORMAL: _bindgen_ty_1 = 4;", "")
         .replace("/**", "/*")
-        .replace("/*!", "/*");
-
-    let mut file = File::create(output).unwrap();
-
-    let _ = file.write(s.as_bytes());
-}
-
-fn common_builder() -> bindgen::Builder {
-    bindgen::builder()
-        .raw_line("#![allow(deprecated)]")
-        .raw_line("#![allow(dead_code)]")
-        .raw_line("#![allow(non_camel_case_types)]")
-        .raw_line("#![allow(non_snake_case)]")
-        .raw_line("#![allow(non_upper_case_globals)]")
+        .replace("/*!", "/*")
 }
 
 fn main() {
     let libs = metadeps::probe().unwrap();
+    let openh264 = libs.get("libavcodec").unwrap();
+    let headers = openh264.include_paths.clone();
 
-    for e in ["avcodec", "avfilter", "avformat", "avresample", "swresample"].iter() {
-        let headers = libs
-            .get(&format!("lib{}", e))
-            .unwrap()
-            .include_paths
-            .clone();
+    let mut builder = bindgen::builder()
+        .header("data/libavcodec.h")
+        .derive_default(true)
+        .derive_debug(true);
 
-        let mut builder = common_builder().header(format!("data/lib{}.h", e));
-
-        for header in headers.iter() {
-            builder = builder.clang_arg("-I").clang_arg(header.to_str().unwrap());
-        }
-
-        // Manually fix the comment so rustdoc won't try to pick them
-        format_write(builder, &format!("src/{}.rs", e));
+    for header in headers {
+        builder = builder.clang_arg("-I").clang_arg(header.to_str().unwrap());
     }
+
+    let s = fmt_write(builder);
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let mut file = File::create(out_path.join("libavcodec.rs")).unwrap();
+    let _ = file.write(s.as_bytes());
 }
